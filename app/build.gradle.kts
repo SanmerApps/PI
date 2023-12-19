12aab8b3c2b9edfc0efde58b0f31ef16f894500a
SanmerDev <sanmer@sanmer.dev>
2023-12-19 16:16:34 +0800

    Bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     alias(libs.plugins.rikka.refine)
 }
 
-val baseVersionName = "0.2.1"
+val baseVersionName = "0.3.0"
 val isDevVersion get() = exec("git tag --contains HEAD").isEmpty()
 val verNameSuffix get() = if (isDevVersion) ".dev" else ""
 
