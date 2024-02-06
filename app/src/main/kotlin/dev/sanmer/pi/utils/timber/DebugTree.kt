b0e2470a811c32a04e959c3e319ab325180187c0
SanmerDev <sanmer@sanmer.dev>
2024-02-06 18:15:47 +0800

    Update logging tag

--- app/src/main/kotlin/dev/sanmer/pi/utils/timber/DebugTree.kt
+++ app/src/main/kotlin/dev/sanmer/pi/utils/timber/DebugTree.kt
@@ -4,12 +4,7 @@ import timber.log.Timber
 
 class DebugTree : Timber.DebugTree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        val newTag = tag?.let {
-            val threadName = Thread.currentThread().name
-            return@let "<PI_DEBUG><$threadName>$tag"
-        }
-
-        super.log(priority, newTag, message, t)
+        super.log(priority, "<PI_DEBUG>$tag", message, t)
     }
 
     override fun createStackElementTag(element: StackTraceElement): String {
