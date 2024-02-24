4726715d7d4baa421a584fefd0b815b4b7314d02
SanmerDev <sanmer@sanmer.dev>
2024-02-24 19:08:36 +0800

    Use `externalCacheDir`
    No access to cacheDir in adb mode

--- app/src/main/kotlin/dev/sanmer/pi/utils/extensions/ContextExt.kt
+++ app/src/main/kotlin/dev/sanmer/pi/utils/extensions/ContextExt.kt
@@ -2,7 +2,7 @@ package dev.sanmer.pi.utils.extensions
 
 import android.content.Context
 
-val Context.tmpDir get() = cacheDir.resolve("tmp")
+val Context.tmpDir get() = checkNotNull(externalCacheDir).resolve("tmp")
     .apply {
         if (!exists()) mkdirs()
     }
