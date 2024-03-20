d556225df9575a07148aa1a2a171db75ffa30fcb
SanmerDev <sanmer@sanmer.dev>
2024-03-20 17:58:00 +0800

    Bump version to 0.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.protobuf)
 }
 
-val baseVersionName = "0.3.4"
+val baseVersionName = "0.3.5"
 val isDevVersion get() = exec("git tag --contains HEAD").isEmpty()
 val verNameSuffix get() = if (isDevVersion) ".dev" else ""
 
