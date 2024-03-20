d047f27af18ea6a73c5b7b436c2d830f788cd623
SanmerDev <sanmer@sanmer.dev>
2024-03-20 17:38:44 +0800

    Update `resourceConfigurations`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         versionName = "${baseVersionName}${verNameSuffix}.${commitId}"
         versionCode = commitCount
 
-        resourceConfigurations += arrayOf("en", "zh-rCN")
+        resourceConfigurations += arrayOf("en", "pt", "pt-rBR", "zh-rCN")
     }
 
     val releaseSigning = if (project.hasReleaseKeyStore) {
