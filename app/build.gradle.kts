bdded8f0897c660a7bb0d1a6bffb4c2bc5eb015e
SanmerDev <sanmer@sanmer.dev>
2024-04-09 15:30:44 +0800

    Bump version to 0.3.7-beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.protobuf)
 }
 
-val baseVersionName = "0.3.6"
+val baseVersionName = "0.3.7-beta01"
 val isDevVersion get() = exec("git tag --contains HEAD").isEmpty()
 val verNameSuffix get() = if (isDevVersion) ".dev" else ""
 
