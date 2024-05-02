55d903e22c2eba48b93065821844af95094555f3
SanmerDev <sanmer@sanmer.dev>
2024-05-02 14:21:26 +0800

    Tidy up code

--- compat/src/main/kotlin/dev/sanmer/hidden/compat/PackageInfoCompat.kt
+++ compat/src/main/kotlin/dev/sanmer/hidden/compat/PackageInfoCompat.kt
@@ -11,53 +11,53 @@ object PackageInfoCompat {
 
     var PackageInfo.versionCodeMajor: Int
         get() = Refine.unsafeCast<PackageInfoHidden>(this).versionCodeMajor
-        set(v: Int) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { versionCodeMajor = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { versionCodeMajor = v }
 
     var PackageInfo.isStub: Boolean
         get() = Refine.unsafeCast<PackageInfoHidden>(this).isStub
-        set(v: Boolean) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { isStub = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { isStub = v }
 
     var PackageInfo.coreApp: Boolean
         get() = Refine.unsafeCast<PackageInfoHidden>(this).coreApp
-        set(v: Boolean) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { coreApp = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { coreApp = v }
 
     var PackageInfo.requiredForAllUsers: Boolean
         get() = Refine.unsafeCast<PackageInfoHidden>(this).requiredForAllUsers
-        set(v: Boolean) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { requiredForAllUsers = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { requiredForAllUsers = v }
 
     var PackageInfo.restrictedAccountType: String?
         get() = Refine.unsafeCast<PackageInfoHidden>(this).restrictedAccountType
-        set(v: String?) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { restrictedAccountType = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { restrictedAccountType = v }
 
     var PackageInfo.requiredAccountType: String?
         get() = Refine.unsafeCast<PackageInfoHidden>(this).requiredAccountType
-        set(v: String?) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { requiredAccountType = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { requiredAccountType = v }
 
     var PackageInfo.overlayTarget: String?
         get() = Refine.unsafeCast<PackageInfoHidden>(this).overlayTarget
-        set(v: String?) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { overlayTarget = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { overlayTarget = v }
 
     var PackageInfo.overlayCategory: String?
         get() = Refine.unsafeCast<PackageInfoHidden>(this).overlayCategory
-        set(v: String?) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { overlayCategory = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { overlayCategory = v }
 
     var PackageInfo.overlayPriority: Int
         get() = Refine.unsafeCast<PackageInfoHidden>(this).overlayPriority
-        set(v: Int) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { overlayPriority = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { overlayPriority = v }
 
     var PackageInfo.compileSdkVersion: Int
         get() = Refine.unsafeCast<PackageInfoHidden>(this).compileSdkVersion
-        set(v: Int) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { compileSdkVersion = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { compileSdkVersion = v }
 
     var PackageInfo.compileSdkVersionCodename: String?
         get() = Refine.unsafeCast<PackageInfoHidden>(this).compileSdkVersionCodename
-        set(v: String?) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { compileSdkVersionCodename = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { compileSdkVersionCodename = v }
 
     var PackageInfo.isActiveApex: Boolean
         @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
         get() = Refine.unsafeCast<PackageInfoHidden>(this).isActiveApex
         @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-        set(v: Boolean) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { isActiveApex = v }
+        set(v) = with(Refine.unsafeCast<PackageInfoHidden>(this)) { isActiveApex = v }
 
     val PackageInfo.isOverlayPackage
         get() = Refine.unsafeCast<PackageInfoHidden>(this).isOverlayPackage
