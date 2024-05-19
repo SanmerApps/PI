7ab722e76cbf14dd943271972391e9641bfd94f6
SanmerDev <sanmer@sanmer.dev>
2024-05-19 21:37:33 +0800

    Update `Const.VERSION_CODE`

--- compat/src/main/kotlin/dev/sanmer/hidden/compat/Const.kt
+++ compat/src/main/kotlin/dev/sanmer/hidden/compat/Const.kt
@@ -5,7 +5,7 @@ import dev.sanmer.hidden.compat.delegate.ContextDelegate
 internal object Const {
     const val TIMEOUT_MILLIS = 15_000L
 
-    const val VERSION_CODE = 3
+    const val VERSION_CODE = 50
 
     val PACKAGE_NAME: String by lazy {
         val context = ContextDelegate.getContext()
