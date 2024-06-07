88b4c96bc26d82cf76f6293e4cea1811c2aed794
SanmerDev <sanmer@sanmer.dev>
2024-06-07 20:19:35 +0800

    Add `fillMaxSize`

--- app/src/main/kotlin/dev/sanmer/pi/ui/screens/apps/details/DetailsScreen.kt
+++ app/src/main/kotlin/dev/sanmer/pi/ui/screens/apps/details/DetailsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -73,7 +74,7 @@ fun DetailsScreen(
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxWidth()
+                .fillMaxSize()
                 .verticalScroll(rememberScrollState())
         ) {
             if (viewModel.hasOpInstallPackage) {
