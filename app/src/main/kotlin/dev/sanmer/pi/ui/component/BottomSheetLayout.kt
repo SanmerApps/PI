5838772a62a14043bf3d076344b58a92797ba55d
SanmerDev <sanmer@sanmer.dev>
2024-06-07 19:16:32 +0800

    Optimize `BottomSheetLayout`
    - Rename from `BottomBarLayout`

--- app/src/main/kotlin/dev/sanmer/pi/ui/component/BottomSheetLayout.kt
+++ app/src/main/kotlin/dev/sanmer/pi/ui/component/BottomSheetLayout.kt
@@ -1,41 +1,63 @@
 package dev.sanmer.pi.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.SubcomposeLayout
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastMap
 import androidx.compose.ui.util.fastMaxBy
 
 @Composable
-fun BottomBarLayout(
+fun BottomSheetLayout(
     modifier: Modifier = Modifier,
-    containerColor: Color = MaterialTheme.colorScheme.surface,
+    containerColor: Color = BottomSheetDefaults.ContainerColor,
     contentColor: Color = contentColorFor(containerColor),
-    tonalElevation: Dp = 0.dp,
-    contentWindowInsets: WindowInsets = WindowInsets.systemBars,
-    bottomBar: @Composable (PaddingValues) -> Unit,
+    shape: Shape = BottomSheetDefaults.ExpandedShape,
+    scrimColor: Color = BottomSheetDefaults.ScrimColor,
+    contentWindowInsets: WindowInsets = BottomSheetDefaults.windowInsets,
+    bottomBar: @Composable (PaddingValues) -> Unit = {},
     content: @Composable (PaddingValues) -> Unit
-) = Surface(
-    modifier = modifier,
-    contentColor = contentColor,
-    color = containerColor,
-    tonalElevation = tonalElevation
+) = Column(
+    modifier = modifier
+        .background(scrimColor)
+        .fillMaxSize(),
+    verticalArrangement = Arrangement.Bottom
 ) {
-    SubcomposeLayout { constraints ->
+    var edgeToTop by remember { mutableStateOf(false) }
+
+    SubcomposeLayout(
+        modifier = Modifier
+            .background(
+                color = containerColor,
+                shape = shape
+            )
+            .clip(shape)
+    ) { constraints ->
+        val maxWidth = constraints.maxWidth
+        val maxHeight = constraints.maxHeight
+
         val insets = contentWindowInsets.asPaddingValues(this)
-        val insetsTop = insets.calculateTopPadding()
+        val insetsTop = if (edgeToTop) insets.calculateTopPadding() else 0.dp
         val insetsBottom = insets.calculateBottomPadding()
         val insetsStart = insets.calculateStartPadding(layoutDirection)
         val insetsEnd = insets.calculateStartPadding(layoutDirection)
@@ -49,7 +71,11 @@ fun BottomBarLayout(
                 end = insetsEnd
             )
 
-            bottomBar(innerPadding)
+            CompositionLocalProvider(
+                LocalContentColor provides contentColor
+            ) {
+                bottomBar(innerPadding)
+            }
         }.fastMap { it.measure(looseConstraints) }
 
         val bottomBarHeight = bottomBarPlaceable.fastMaxBy { it.height }?.height ?: 0
@@ -62,12 +88,17 @@ fun BottomBarLayout(
                 end = insetsEnd
             )
 
-            content(innerPadding)
+            CompositionLocalProvider(
+                LocalContentColor provides contentColor
+            ) {
+                content(innerPadding)
+            }
         }.fastMap { it.measure(looseConstraints) }
 
         val bodyContentHeight = bodyContentPlaceable.fastMaxBy { it.height }?.height ?: 0
+        edgeToTop = bodyContentHeight >= maxHeight
 
-        layout(constraints.maxWidth, bodyContentHeight) {
+        layout(maxWidth, bodyContentHeight) {
             bodyContentPlaceable.fastForEach {
                 it.place(
                     x = 0,
