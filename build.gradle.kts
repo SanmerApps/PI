a363f599b58d3b5ef616e8c8646248e1582f097f
SanmerDev <sanmer@sanmer.dev>
2024-07-22 14:21:27 +0800

    Bump version to 1.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ task<Delete>("clean") {
 }
 
 subprojects {
-    val baseVersionName by extra("1.1.2")
+    val baseVersionName by extra("1.1.3")
 
     apply(plugin = "maven-publish")
     configure<PublishingExtension> {
