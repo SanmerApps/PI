2874b46cf3163731ba412de11c80d360d3c4d601
SanmerDev <sanmer@sanmer.dev>
2024-07-24 19:46:23 +0800

    Optimize `BuildCompat`

--- app/src/main/kotlin/dev/sanmer/pi/compat/BuildCompat.kt
+++ app/src/main/kotlin/dev/sanmer/pi/compat/BuildCompat.kt
@@ -5,14 +5,11 @@ import androidx.annotation.ChecksSdkIntAtLeast
 
 object BuildCompat {
     @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-    val atLeastU get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+    val atLeastU inline get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
 
     @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU)
-    val atLeastT get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+    val atLeastT inline get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
 
     @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
-    val atLeastS get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-
-    @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
-    val atLeastR get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+    val atLeastS inline get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
 }
