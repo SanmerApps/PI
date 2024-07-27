0f3348cc698167522bce180a8fc6546625d29d4c
SanmerDev <sanmer@sanmer.dev>
2024-07-28 00:09:44 +0800

    Fix `UserPreferences`

--- app/src/main/kotlin/dev/sanmer/pi/datastore/model/UserPreferences.kt
+++ app/src/main/kotlin/dev/sanmer/pi/datastore/model/UserPreferences.kt
@@ -5,13 +5,17 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import kotlinx.serialization.protobuf.ProtoBuf
+import kotlinx.serialization.protobuf.ProtoNumber
 import java.io.InputStream
 import java.io.OutputStream
 
 @Serializable
 data class UserPreferences(
+    @ProtoNumber(1)
     val provider: Provider = Provider.None,
+    @ProtoNumber(3)
     val requester: String = BuildConfig.APPLICATION_ID,
+    @ProtoNumber(4)
     val executor: String = BuildConfig.APPLICATION_ID
 ) {
     fun encodeTo(output: OutputStream) = output.write(
