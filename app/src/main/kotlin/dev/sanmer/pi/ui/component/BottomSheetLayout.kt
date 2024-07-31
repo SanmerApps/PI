04a3e66575e4cca9931e13aca7d25813aeed0df1
SanmerDev <sanmer@sanmer.dev>
2024-07-31 22:36:41 +0800

    Optimize `BottomSheetLayout`

--- app/src/main/kotlin/dev/sanmer/pi/ui/component/BottomSheetLayout.kt
+++ app/src/main/kotlin/dev/sanmer/pi/ui/component/BottomSheetLayout.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.contentColorFor
@@ -17,11 +19,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastMap
@@ -32,6 +36,7 @@ fun BottomSheetLayout(
     modifier: Modifier = Modifier,
     containerColor: Color = BottomSheetDefaults.ContainerColor,
     contentColor: Color = contentColorFor(containerColor),
+    sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
     shape: Shape = BottomSheetDefaults.ExpandedShape,
     scrimColor: Color = BottomSheetDefaults.ScrimColor,
     contentWindowInsets: WindowInsets = BottomSheetDefaults.windowInsets,
@@ -41,12 +46,15 @@ fun BottomSheetLayout(
     modifier = modifier
         .background(scrimColor)
         .fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Bottom
 ) {
     var edgeToTop by remember { mutableStateOf(false) }
 
     SubcomposeLayout(
         modifier = Modifier
+            .widthIn(max = sheetMaxWidth)
+            .fillMaxWidth()
             .background(
                 color = containerColor,
                 shape = shape
