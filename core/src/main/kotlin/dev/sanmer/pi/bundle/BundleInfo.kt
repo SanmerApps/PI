4f6855bee4e7a4edfa433c98d32c8db2a9ebf190
SanmerDev <sanmer@sanmer.dev>
2025-03-20 16:32:50 +0800

    Impl Parcelable for `BundleInfo`

--- core/src/main/kotlin/dev/sanmer/pi/bundle/BundleInfo.kt
+++ core/src/main/kotlin/dev/sanmer/pi/bundle/BundleInfo.kt
@@ -1,10 +1,13 @@
 package dev.sanmer.pi.bundle
 
 import android.content.pm.PackageInfo
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.io.File
 
+@Parcelize
 data class BundleInfo(
     val baseFile: File,
     val baseInfo: PackageInfo,
     val splitConfigs: List<SplitConfig>
-)
+) : Parcelable
