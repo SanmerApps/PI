501be165c6a11a392621e172f4f7fb703fd96f5d
SanmerDev <sanmer@sanmer.dev>
2026-09-13 19:49:34 +0800

    Bump version to 2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
-val baseVersionName = "2.0.1"
+val baseVersionName = "2.0.2"
 val gitCommitTag = gitCommitTag()
 val gitCommitSha = gitCommitSha()
 val gitCommitNum = gitCommitNum()
